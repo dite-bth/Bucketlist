0104be3c62c4136974f8675af970ec540ea80399
idsv14 <ida.svensson.is@icloud.com>
2016-09-30 10:50:12 +0200

    working?

--- server/main.py
+++ server/main.py
@@ -1,22 +1,33 @@
 # all the imports
-import os
-import sqlite3
+import os, sys, json
+import sqlite3 as lite
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
+from user import User
 
+conn = lite.connect('bucketlist.db')
 
 # create our little application :)
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route("/profile")
-def profile():
-    return render_template("profile.html")
 
-@app.route("/index")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    return 'hejsan'
 
+@app.route("/profile/<userid>")
+def profile(userid):
+    user = User(dbcon, userid)
+    return render_template("profile.html", userid=userid)
+
+@app.route("/main")
+def main():
+    return render_template("main.html")
+
+@app.route("/signin")
+def signin():
+    return render_template("signin/index.html")
 
 if __name__ == '__main__':
     app.run()
