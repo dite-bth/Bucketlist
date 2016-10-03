05473a376e022a87de35c071ceadd877e2f146f4
Johannapersson <perssonjohanna@gmail.com>
2016-10-03 14:03:02 +0200

    borta

--- login/key.py
+++ login/key.py
@@ -1,3 +1,3 @@
-GOOGLE_CLIENT_ID = '43138052863-aqqjblmd6v5dj8fsg5umpo9e6ek8jk71.apps.googleusercontent.com'
-GOOGLE_CLIENT_SECRET = 'sNUu5ea7wiWtvjxzTm-_-4Xn'
+GOOGLE_CLIENT_ID = ''
+GOOGLE_CLIENT_SECRET = ''
 REDIRECT_URI = "/oauthcallback"  # one of the Redirect URIs from Google APIs console
