2d703175a49afe66612370b1d8ec31ff9b6dc482
T-bear <torbjorn.holgersson87@gmail.com>
2016-10-04 15:39:39 +0200

    nya klass och något annat

---
+++ server/bucketlist.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from trick import Trick
+import sqlite3 as lite
+
+
+conn = lite.connect(bucketlist.db)
+
+trickid = getTrickId
+
+    if trickid == 0:
+        trickid = getTrickId
+    else:
+        trickid = getTrickId +1
+        print trickid
+
+
+
+
+        value1 = nick
---
+++ server/trick.py
@@ -0,0 +1,27 @@
+import sqlite3 as lite
+import sys
+
+
+class Trick:
+    def __init__(self, trickid, name=None, url=None, level=None):
+        self.trick_name = ""
+        self.trick_url = ""
+        self.level = ""
+        self.trick_id = trickid
+
+        if name == None:
+            self.dbcon = lite.connect('bucketlist.db')
+            cursor = self.dbcon.cursor()
+            cursor.execute("SELECT * FROM trikslist WHERE trick_id=?",(trickid))
+            result = cursor.fetchone()
+            self.trick_name = result[1]
+        else:
+            self.trick_name = name
+            self.trick_url = url
+            self.level = level
+
+    def getName(self):
+        return 0
+
+
+
