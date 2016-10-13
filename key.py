b6fc4be6cb180f1c4bcaabd2ff00c6da0fcc6cdc
xsand <andreasandersson0031@gmail.com>
2016-10-13 12:39:07 +0200

    reg
    registrering woking

---
+++ key.py
@@ -0,0 +1,4 @@
+GOOGLE_CLIENT_ID = '43138052863-aqqjblmd6v5dj8fsg5umpo9e6ek8jk71.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'sNUu5ea7wiWtvjxzTm-_-4Xn'
+REDIRECT_URI = "/oauthcallback"  # one of the Redirect URIs from Google APIs console
+
